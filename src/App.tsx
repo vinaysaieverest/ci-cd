adc38659f3825c8b95ddf50135bc21cc99b1ecba
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-25 14:54:40 +0530

    Add basic test

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>
+      Hello, I am Vinay sai Chelupuri.
+    </h1>
+    <h1>
+    Let's test
+    </h1>
+    </div>
+  );
+}
+
+export default App;
